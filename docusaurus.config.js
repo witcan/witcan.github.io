617331f4891d3eaf589661b5363d524e894542af
aiyuhang <yh.ai@qq.com>
2025-12-30 10:31:58 +0800

    feat: 增加自动化部署

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,8 +27,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'witcan', // Usually your GitHub org/user name.
+  projectName: 'witcan.github.io', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
